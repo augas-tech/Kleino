1601c8f05f6bb33c0df0c15a6fcf0e5f35c77cbd
Agustin Ugas <augas@utem.cl>
2026-09-19 16:06:18 -0300

    Agrega autenticacion y modelos base: User,Sede,Espacio,Reserva

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   # Health check por defecto de Rails (200 si la app bootea sin excepciones).
   get "up" => "rails/health#show", as: :rails_health_check
 
